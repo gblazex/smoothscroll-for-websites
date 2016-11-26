c97a7db2adced6510e61c83d24e7b00cbabf8850
Balázs Galambosi <galambalazs@yahoo.co.uk>
2016-11-26 23:00:34 +0100

    remove "touchpadSupport" legacy option
    touchpad is best “supported” by not messing with it (so keeping this
    option disabled)

--- SmoothScroll.js
+++ SmoothScroll.js
@@ -38,7 +38,6 @@ var defaultOptions = {
     arrowScroll       : 50,    // [px]
 
     // Other
-    touchpadSupport   : false, // ignore touchpad by default
     fixedBackground   : true, 
     excluded          : ''    
 };
@@ -349,7 +348,7 @@ function wheel(event) {
     }
     
     // check if it's a touchpad scroll that should be ignored
-    if (!options.touchpadSupport && isTouchpad(deltaY)) {
+    if (isTouchpad(deltaY)) {
         return true;
     }
 
