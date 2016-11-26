c9d0e83c08fe52a17b06481bff45b1d5a3bd6b18
Balázs Galambosi <galambalazs@yahoo.co.uk>
2016-11-26 23:04:19 +0100

    bump version 1.4.6

--- SmoothScroll.js
+++ SmoothScroll.js
@@ -1,5 +1,5 @@
 //
-// SmoothScroll for websites v1.4.5 (Balazs Galambosi)
+// SmoothScroll for websites v1.4.6 (Balazs Galambosi)
 // http://www.smoothscroll.net/
 //
 // Licensed under the terms of the MIT license.
