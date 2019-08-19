4110d500b2157554754087698a595f10f8a9ced8
Blaze <galambalazs@yahoo.co.uk>
2019-08-19 11:08:05 +0200

    bump version 1.4.10

--- SmoothScroll.js
+++ SmoothScroll.js
@@ -1,5 +1,5 @@
 //
-// SmoothScroll for websites v1.4.9 (Balazs Galambosi)
+// SmoothScroll for websites v1.4.10 (Balazs Galambosi)
 // http://www.smoothscroll.net/
 //
 // Licensed under the terms of the MIT license.
